928228bd2ea7a7d1bfafde6db56a92875d162132
Madeline Wong <madelinegwong@outlook.com>
2018-01-29 18:54:50 -0500

    learned how to create a variable.

---
+++ variables-and-datatypes.js
@@ -0,0 +1,12 @@
+// single line comment 
+/**
+ * This is a multi-line comment
+ */
+
+ //Variables 
+ var name= "Madeline"; //old school javascript(don't use this.)
+ const fruit= "Banana"; //constant binding (lock it in the cup)
+ let veggie= "Carrot";// similar to Var, but some other differences...
+ console.log("Name: ", name) //print
+ console.log("Fruit: ", fruit);
+ console.log("Veggie: ", veggie);
